f9a7cda6829354d06db28d0121142d4358969198
Sakhawath066 <sakhawath066@gmail.com>
2024-03-18 12:35:54 +0600

    nested if

---
+++ Nested If/number greater.py
@@ -0,0 +1,10 @@
+x=int(input('enter number:'))
+
+if x>10:
+    print('it is greater than 10')
+    if x>20:
+        print('it is also greater than 20')
+    else:
+        print ('but it is not greater than 20')
+else:
+    print('it is less than 10')
