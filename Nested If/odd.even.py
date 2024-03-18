32d39311ef4c8cd90261d3f9a5b2171f79c3e9ac
Sakhawath066 <sakhawath066@gmail.com>
2024-03-18 13:05:33 +0600

    2nd nested if

---
+++ Nested If/odd.even.py
@@ -0,0 +1,8 @@
+num = 33
+if num > 0:
+  if num % 2 == 0:
+    print("The number is positive and even.")
+  else:
+    print("The number is positive but odd.")
+else:
+  print("The number is not positive.")
