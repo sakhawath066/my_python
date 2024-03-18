f6211472c101d66def043f774951c5962ff2a516
Sakhawath066 <sakhawath066@gmail.com>
2024-03-19 00:12:08 +0600

    number zero,positive or negative

---
+++ Nested If/number is zero, positive, or negative.py
@@ -0,0 +1,11 @@
+## Check whether a number is zero, positive, or negative.
+
+x=int(input('Enter the number: '))
+
+if x>=0:
+    if x==0:
+        print('it is a Zero number')
+    else:
+        print('it is positive number')
+else:
+    print('it is negative')
