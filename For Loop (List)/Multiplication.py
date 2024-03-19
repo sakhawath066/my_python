f4f8e8fc308f520164e6b659e9f799f3dfa29a99
Sakhawath066 <sakhawath066@gmail.com>
2024-03-19 19:19:14 +0600

    multiplication table

---
+++ For Loop (List)/Multiplication.py
@@ -0,0 +1,5 @@
+d = int(input('Enter the multiplication number: '))
+i = 0
+while i < 10:
+   i+=1
+   print(i, "x", d, '=', d * i)
--- While Loop/Multiplication.py
+++
@@ -1,3 +0,0 @@
-call = 5
-for i in range(1, 11):
-   print(num, 'x', i, '=', num*i)
