25a68a1c5dca2bdbc99b3c93b8868823bcd85560
Sakhawath066 <sakhawath066@gmail.com>
2024-03-22 15:20:12 +0600

    for loop

--- For Loop (List)/1st for.py
+++ For Loop (List)/1st for.py
@@ -1,3 +1,3 @@
-name=['sakhawath','tazeem','tamzeed','tawwab']
+name=['1','2','3','4', '5','6']
 for d in name:
     print(d)
---
+++ For Loop (List)/test.py
@@ -0,0 +1,7 @@
+fruits = ['banana', 'orange', 'apple', 'melon', 'mango']
+name = ['sakhawath', 'tazeem', 'tamzeed', 'tawwab']
+
+for f in fruits:
+    for n in name:
+        print(f,n)
+        
